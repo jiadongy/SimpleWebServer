62191ce6440f35dc5353c15374dad0ceb04b9c33
jiadongy <jiadongy@163.com>
2015-06-25 02:32:39 +0800

    add spring and log4j

---
+++ src/test/callbackmode/file/FileManagerTest.java
@@ -0,0 +1,45 @@
+package test.callbackmode.file; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* FileManager Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ÁùÔÂ 22, 2015</pre> 
+* @version 1.0 
+*/ 
+public class FileManagerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getInstance() 
+* 
+*/ 
+@Test
+public void testGetInstance() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getFileContent(File fileToGet) 
+* 
+*/ 
+@Test
+public void testGetFileContent() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
